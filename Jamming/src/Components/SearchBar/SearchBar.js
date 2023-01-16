44a7f3e5ef494e8cf1b27d6f620760978ec91033
Alvaro-LGPA <alvarolaura@gmail.com>
2023-01-16 11:54:41 +0100

    Ready to hook to Spotify

--- Jamming/src/Components/SearchBar/SearchBar.js
+++ Jamming/src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,28 @@ import React from "react";
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleSearchTerm = this.handleSearchTerm.bind(this);
+
+        this.state ={
+            term: ''
+        }
+    }
+
+    search(){
+        this.props.onSearch(this.state.term)
+    }
+
+    handleSearchTerm(event){
+        this.setState({term: event.target.value});
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleSearchTerm}/>
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
